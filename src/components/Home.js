965dbc869e3712ca7066c27dce4860cfeaba0a3c
Kevin-S <kevsilvo@gmail.com>
2019-07-10 17:13:36 -0400

    created new files components

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <div>
+            <h1>Avengers Database</h1>
+            <h4>Danger: This is classiffied Information</h4>
+        </div>
+    );
+
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// In order to initialize the routing we have to wrap our app component
+// with the Router component 
+
+
+ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
